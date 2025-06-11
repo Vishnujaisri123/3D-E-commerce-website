349203d5490963669e132f9e0044432395bdc599
Your NameVishnujiasri123 <youremail@example.comvishnuketa999@gmail.com>
2025-06-11 22:53:12 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base:"/3D-E-commerce-website"
+  base: "/3D-E-commerce-website",
+  build: {
+    chunkSizeWarningLimit: 1500, // Silences big chunk warnings
+  },
 });
